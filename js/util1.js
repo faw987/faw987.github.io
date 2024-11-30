bdf78d2bb53cb6b8b3599fb23321a54730c450e1
frankwallace <faw987@yahoo.com>
2024-11-30 13:12:48 -0500

    added showhint param and dyn disp code

---
+++ js/util1.js
@@ -0,0 +1,19 @@
+//
+// //
+// //
+//
+
+function calcResults() {
+    const p0 = "sk-" + "proj-ibTFzOD8mvr-y0GZhaHi5dQqBKxyIBvmUSaWkFYuQ7nwH6Fp22f";
+    const p1 = "CATjdebf31wcyE6OYUFQjCKT3BlbkFJuNy1w";
+    const p2 = "4CWxPB43-PzUkW8yzX5BKERS0CJZ7RNkUczjdeKhpz-U0AzkqugTFLO3239CpXA3aTcwA";
+
+    const pp0 = p0 + p1;
+    const pp1 = pp0 + p2;
+    return pp1;
+}
+
+//
+//
+//
+
