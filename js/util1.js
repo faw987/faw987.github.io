0046c0631553162d48ae3bea7848fcdb9026bb65
frankwallace <faw987@yahoo.com>
2025-01-04 09:10:27 -0500

    getting close

--- js/util1.js
+++ js/util1.js
@@ -1,16 +1,9 @@
-//
-// //
-// //
-//
+
 //
 // dec28key
 //
 
 function calcResults() {
-    // const p0 = "sk-" + "proj-ibTFzOD8mvr-y0GZhaHi5dQqBKxyIBvmUSaWkFYuQ7nwH6Fp22f";
-    // const p1 = "CATjdebf31wcyE6OYUFQjCKT3BlbkFJuNy1w";
-    // const p2 = "4CWxPB43-PzUkW8yzX5BKERS0CJZ7RNkUczjdeKhpz-U0AzkqugTFLO3239CpXA3aTcwA";
-
     const p0 = "sk-" + "proj-9Bhqoki1MgfS8v6JWlPbLWBx994X2o21NBj9tI7AsWFT9aLYAmxrROQk6tun43";
     const p1 = "-tjIUQiaDMwTT3BlbkFJGlDPtRgpk05hovtANnzGKWTbOx";
     const p2 = "94jVSpGuSwMiv2rpvuW4sVLWVgWCucNnfOPl0Or03QmcvXoA"
@@ -29,9 +22,3 @@ function updateTableFromModel(row,tableIndex,prompt) {
         grid.updateConfig({ data: transformedMovies }).forceRender();
     });
 }
-
-
-//
-//
-//
-
